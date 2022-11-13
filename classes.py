aabbd3b296f1d613199de022a7e03b3a190f6c05
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-13 14:18:59 +0100

    Made line color a class property

--- classes.py
+++ classes.py
@@ -15,8 +15,9 @@ class Resistor:
         self.resistorRect = self.resistorImg.get_rect( center = pos )
 
         self.halfWidth = rectWidth / 2
+        self.lineColor = (192, 192, 192)
 
     def draw(self, screen):
         screen.blit(self.resistorImg, self.resistorRect)
-        pygame.draw.line(screen, (192, 192, 192), (self.resistorRect.left - 150, self.resistorRect.centery), (self.resistorRect.left, self.resistorRect.centery), 20)
-        pygame.draw.line(screen, (192, 192, 192), (self.resistorRect.right + 150, self.resistorRect.centery), (self.resistorRect.right, self.resistorRect.centery), 20)
+        pygame.draw.line(screen, self.lineColor, (self.resistorRect.left - 150, self.resistorRect.centery), (self.resistorRect.left, self.resistorRect.centery), 20)
+        pygame.draw.line(screen, self.lineColor, (self.resistorRect.right + 150, self.resistorRect.centery), (self.resistorRect.right, self.resistorRect.centery), 20)
