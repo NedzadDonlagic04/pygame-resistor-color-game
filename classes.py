38400927ee1b1cde781be9bddb4cc490e50b2f9f
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-13 14:16:59 +0100

    Added class to represent the resistor

--- classes.py
+++ classes.py
@@ -7,3 +7,16 @@ class MyClock:
     
     def tick(self):
         self.CLOCK.tick(self.FPS)
+
+class Resistor:
+    def __init__(self, rectWidth, rectHeight, pos, color=[234, 182, 118]):
+        self.resistorImg = pygame.Surface((rectWidth, rectHeight), pygame.SRCALPHA)
+        pygame.draw.rect(self.resistorImg, color, (0, 0, rectWidth, rectHeight))
+        self.resistorRect = self.resistorImg.get_rect( center = pos )
+
+        self.halfWidth = rectWidth / 2
+
+    def draw(self, screen):
+        screen.blit(self.resistorImg, self.resistorRect)
+        pygame.draw.line(screen, (192, 192, 192), (self.resistorRect.left - 150, self.resistorRect.centery), (self.resistorRect.left, self.resistorRect.centery), 20)
+        pygame.draw.line(screen, (192, 192, 192), (self.resistorRect.right + 150, self.resistorRect.centery), (self.resistorRect.right, self.resistorRect.centery), 20)
